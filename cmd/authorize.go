c594ef9e52cce3c1f0b9421f86e017550097bad6
DJ Patterson <david.patterson@shopify.com>
2021-07-31 22:10:10 +0100

    Add Authorize command

---
+++ cmd/authorize.go
@@ -0,0 +1,38 @@
+/*
+Copyright © 2021 DJ Patterson
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/davidPatters0n/imgurcli/pkg/authorization"
+	"github.com/spf13/cobra"
+)
+
+// authorizeCmd represents the account command
+var authorizeCmd = &cobra.Command{
+	Use:   "authorize",
+	Short: "Authorize your account",
+
+	Run: func(cmd *cobra.Command, args []string) {
+		account := authorization.Authorize()
+		fmt.Println("authorized account - %s", account)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(authorizeCmd)
+}
