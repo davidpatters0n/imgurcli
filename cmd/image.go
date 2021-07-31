a4b3149e75cd3d3c45c17ab80137641b761714cb
DJ Patterson <david.patterson@shopify.com>
2021-07-31 17:32:34 +0100

    Add Root command & subcomamnds (Image / ImageFetch)

---
+++ cmd/image.go
@@ -0,0 +1,39 @@
+/*
+Copyright © 2021 DJ Patterson
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// imageCmd represents the image command
+var imageCmd = &cobra.Command{
+	Use:   "image",
+	Short: "Interacts with the Image API",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			cmd.Help()
+			os.Exit(0)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(imageCmd)
+	imageCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
